f414537f0fe841caaf3f6b0c4d6fe2978c6dac5b
0xjuju <jermol.jupiter@gmail.com>
2024-11-12 08:05:54 -0500

    Initialize agents app for LLM configurations

--- _settings/settings.py
+++ _settings/settings.py
@@ -52,7 +52,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'telegram_api'
+    'telegram_api',
+    'agents'
 ]
 
 MIDDLEWARE = [
